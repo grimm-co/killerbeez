240617ff9ec642c77f84c709b649cdb4f28d2619
jeffball <jeffball@grimm-co.com>
2018-03-23 16:18:00 -0400

    Updated the driver_t and mutator_t structures to reflect the new API functions

--- docs/files/driver_t.c
+++ docs/files/driver_t.c
@@ -2,6 +2,8 @@ struct driver
 {
   void (*cleanup)(void * driver_state);
   int (*test_input)(void * driver_state, char * buffer, size_t length);
+  int(*test_next_input)(void * driver_state);
+  void *(*get_last_input)(void * driver_state, int * length);
   void * state;
 };
 typedef struct driver driver_t;
--- docs/files/mutator_t.c
+++ docs/files/mutator_t.c
@@ -7,11 +7,15 @@ typedef struct mutator
     size_t buffer_length);
   int(*mutate_thread_safe)(void * mutator_state, char * buffer,
     size_t buffer_length);
+  int(*mutate_input_part)(void * mutator_state, int input_part, char * buffer,
+    size_t buffer_length);
   char * (*get_state)(void * mutator_state);
   void(*free_state)(char * state);
   int(*set_state)(void * mutator_state, char * state);
   int(*get_current_iteration)(void * mutator_state);
   int(*get_total_iteration_count)(void * mutator_state);
+  void(*get_input_info)(void * mutator_state, int * num_inputs,
+    size_t **input_sizes);
   int(*set_input)(void * mutator_state, char * new_input,
     size_t input_length);
   int(*help)(char **help_str);
