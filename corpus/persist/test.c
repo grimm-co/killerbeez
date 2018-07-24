aad84d6650f4299ccd5ad30101868ad6a60be0c4
jeffball <jeffball@grimm-co.com>
2018-07-24 19:59:01 -0400

    Added an example binary for IPT persistence mode

---
+++ corpus/persist/test.c
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifdef PERSIST
+#include "forkserver.h"
+#endif
+
+int test_func()
+{
+	char buffer[4];
+	char * nil = NULL;
+	FILE * fp = stdin;
+	memset(buffer, 0, 4);
+  read(0, buffer, sizeof(buffer));
+
+	if (buffer[0] == 'A')
+	{
+		if (buffer[1] == 'B')
+		{
+			if (buffer[2] == 'C')
+			{
+				if (buffer[3] == 'D')
+				{
+					*nil = 'E';
+				}
+				else
+				{
+					puts("Wrong 3");
+				}
+			}
+			else
+			{
+				puts("Wrong 2");
+			}
+		}
+		else
+		{
+			puts("Wrong 1");
+		}
+	}
+	else
+	{
+		puts("Wrong 0");
+	}
+
+  return 0;
+}
+
+
+int main()
+{
+#ifdef PERSIST
+  while(KILLERBEEZ_LOOP()) {
+#endif
+    test_func();
+#ifdef PERSIST
+  }
+#endif
+
+  return 0;
+}
