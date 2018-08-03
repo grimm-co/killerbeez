b6ff283b1f871b15b0ed32c4bee3748df53ef14c
Ben Lipton <ben@grimm-co.com>
2018-08-03 14:49:23 -0500

    Ensure result zip is never empty, because wrapper won't generate it

--- server/skel/windows_x86_64/flatten_results.ps1
+++ server/skel/windows_x86_64/flatten_results.ps1
@@ -1,3 +1,4 @@
+echo "Results from a killerbeez run. This file ensures an empty zip file is not generated." > README.txt
 Foreach($type in "crashes", "hangs", "new_paths") {
   Get-ChildItem output\$type |
   Foreach-Object {
